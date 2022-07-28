d98bf4322194d4e735211b21eb3833759d100f19
komronz <pandacoder0820@gmail.com>
2022-07-28 14:42:31 +0500

    moduleTask2

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,12 +3,12 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable1 = 2000;
+        int variable_1 = 2000;
         int value = 1000;
-        int variable2 = 4000;
-        int value2 = 3000;
+        int variable_2 = 4000;
+        int value_2 = 3000;
         int variables = 5000;
         variables = 7000;
-        int value1 = 90000;
+        int value_1 = 90000;
     }
 }
--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -3,13 +3,13 @@ package lang.print.gaps.task2;
 public class NamingConvention {
     public static void main(String[] args) {
         int variable = 10;
-        int variable1 = 1;
-        int variable2 = 2;
-        int variable3 = 3;
-        int variable4 = 4;
-        int variable5 = 5;
-        int variable6 = 6;
-        int variable7 = 7;
-        int variable8 = 8;
+        int variableOne= 1;
+        int variableTwo = 2;
+        int variableThree = 3;
+        int variableFour = 4;
+        int variableFive = 5;
+        int variableSix = 6;
+        int variableSeven = 7;
+        int variableEight = 8;
     }
 }
